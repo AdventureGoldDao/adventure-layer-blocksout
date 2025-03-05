72005ca4a8c83a2e1b1e246ce5313799950158a6
willcodex <willcodexyz@amail.com>
2025-03-05 09:34:51 +0800

    fix: remove default value with base path

--- configs/app/al_config.ts
+++ configs/app/al_config.ts
@@ -50,7 +50,7 @@ export const alNetworkConfig: any = {
     port: apiPort || '',
     endpoint: apiEndpoint || 'https://explorer-devnet.adventurelayer.xyz',
     socket: socketEndpoint || 'wss://explorer-devnet.adventurelayer.xyz',
-    basePath: basePath || '/l2',
+    basePath: basePath,
   },
   shard1: {
     host: apiHostShard || 'explorer-devnet.adventurelayer.xyz',
@@ -58,7 +58,7 @@ export const alNetworkConfig: any = {
     port: apiPortShard || '',
     endpoint: apiEndpointShard || 'https://explorer-devnet.adventurelayer.xyz',
     socket: socketEndpointShard || 'wss://explorer-devnet.adventurelayer.xyz',
-    basePath: basePathShard || '/shard1',
+    basePath: basePathShard,
   },
   // shard2: {
   //   host: 'explorer-devnet.adventurelayer.xyz',
